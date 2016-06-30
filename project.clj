9a598972362f04da5a8c1578178ab0d39b9ceb89
kris <kristen.f.jacobs@oracle.com>
2016-06-30 14:41:09 +0100

    Fix xml test output

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins      [[lein-cloverage "1.0.6"]]
+  :plugins      [[lein-cloverage "1.0.6"]
+                 [test2junit "1.2.2"]]
   :main ^:skip-aot circleci-clj-test.core
   :target-path "target/%s"
   :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
